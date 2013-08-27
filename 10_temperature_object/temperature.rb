7a6e1594fd2dde45edfdd406822b6ae1d7df9938
cshah3 <shah.control@gmail.com>
2013-08-27 16:00:29 -0400

    first commit

---
+++ 10_temperature_object/temperature.rb
@@ -0,0 +1,45 @@
+class Temperature
+	attr_accessor :f, :c
+
+	def initialize(h)
+		@hash = h
+	end
+
+	def to_fahrenheit
+		if @hash.include? :f
+			return @hash[:f]
+		else
+			return @hash[:c] * (9.0/5.0) + 32
+		end
+	end
+
+	def to_celsius
+		if @hash.include? :c
+			return @hash[:c]
+		else
+			return (@hash[:f] - 32) * (5.0/9.0)
+		end
+	end
+
+	def self.in_celsius(temp)
+		self.new({:c => temp})
+	end
+
+	def self.in_fahrenheit(temp)
+		self.new({:f => temp})
+	end
+end
+
+class Celsius < Temperature
+	def initialize(temp)
+		@hash = {:c => temp}
+		Temperature.new(@hash)
+	end
+end
+
+class Fahrenheit < Temperature
+	def initialize(temp)
+		@hash = {:f => temp}
+		Temperature.new(@hash)
+	end
+end
